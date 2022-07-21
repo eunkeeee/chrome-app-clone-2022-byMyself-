11ba79a5f825e5196be3dc65139665a46e7ec225
eunkeeee <eunkeeeee@gmail.com>
2022-07-22 00:37:09 +0900

    7.8 까지인데 제대로 작동안함

--- js/todo.js
+++ js/todo.js
@@ -6,15 +6,26 @@ const KEY_TODOS = "todos";
 
 let toDos = [];
 
+function saveToDos(toDos) {
+  localStorage.setItem(KEY_TODOS, JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  // console.log(toDos);
+
+  // 필터의 조건에 걸리는거만 빼고 나머지로 새로은 array를 만듦
+  toDos = toDos.filter((element) => element.id !== parseInt(li.id));
+  // console.log(toDos);
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "X";
   button.addEventListener("click", deleteToDo);
@@ -28,15 +39,21 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  paintToDo(newTodo);
-  toDos.push(newTodo);
-  localStorage.setItem(KEY_TODOS, JSON.stringify(toDos));
+
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(), // delete을 위해 겹치지 않는 id 부여
+  };
+
+  paintToDo(newTodoObj);
+  toDos.push(newTodoObj);
+  saveToDos(toDos);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(KEY_TODOS);
-if (savedToDos) {
+if (savedToDos !== null) {
   // if there is something inside, load todos
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
