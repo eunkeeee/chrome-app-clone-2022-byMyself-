5493984f841a14555149263b0f3cfa20a0744475
eunkeeee <eunkeeeee@gmail.com>
2022-07-21 15:56:37 +0900

    Updated AMPM for clock

--- clock.js
+++ clock.js
@@ -2,10 +2,18 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
   const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
+  let AMPM = "AM";
+  let hours = date.getHours();
+  if (hours < 12) {
+    AMPM = "AM";
+    hours = String(hours).padStart(2, "0");
+  } else {
+    AMPM = "PM";
+    hours = String(hours - 12).padStart(2, "0");
+  }
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  clock.innerText = `${hours}:${minutes}:${seconds} ${AMPM}`;
 }
 
 getClock();
