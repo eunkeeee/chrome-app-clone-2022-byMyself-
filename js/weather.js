d9b770b107567efd5d85d9f249c8b909d154928b
eunkeeee <eunkeeeee@gmail.com>
2022-07-23 10:58:39 +0900

    CSS 작업 1차

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,19 @@
+const weather = document.querySelector(".current_weather");
+const city = document.querySelector(".current_city");
+
+const API_KEY = "468de432391fc4dc7d55c1f7787de6ec";
+
 function onGeoSuccess(position) {
   const lat = position.coords.latitude;
-  const long = position.coords.longitude;
+  const lon = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+    });
 }
 function onGeoError() {
   alert("Weather Unavailable");
