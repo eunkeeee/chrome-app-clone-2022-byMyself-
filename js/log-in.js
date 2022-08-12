9465e6b59dd55a1d9615defc0d9485c87fab5cd5
eunkeeee <eunkeeeee@gmail.com>
2022-08-12 18:20:42 +0900

    로그인과 greeting, todo list 위치 조정 완료

--- js/log-in.js
+++ js/log-in.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const todoList = document.querySelector("#todo-form");
 
 const HIDDEN_CLASSNAME = "hidden";
 const KEY_USERNAME = "username";
@@ -8,7 +9,18 @@ const KEY_USERNAME = "username";
 const savedUsername = localStorage.getItem(KEY_USERNAME);
 const timeGreeting = () => {
   const date = new Date();
+  let insa = "";
   let hours = date.getHours();
+  if (hours < 11) {
+    insa = "Good morning, ";
+  } else if (hours < 15) {
+    insa = "Good afternoon, ";
+  } else if (hours < 20) {
+    insa = "Good evening, ";
+  } else {
+    insa = "Good night, ";
+  }
+  return insa;
 };
 
 function onLoginSubmit(event) {
@@ -20,8 +32,11 @@ function onLoginSubmit(event) {
 }
 function paintGreetings(submittedName) {
   // *** 시간대에 따라 인사말이 다르도록 수정 !!!
-  greeting.innerText = `Hello ${submittedName}`;
+  greeting.innerText = `${timeGreeting()} ${submittedName}`;
+  greeting.style.fontSize = "50px";
+  greeting.style.fontWeight = "530";
   greeting.classList.remove(HIDDEN_CLASSNAME);
+  todoList.classList.remove(HIDDEN_CLASSNAME);
 }
 
 if (savedUsername !== null) {
