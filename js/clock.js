3b669ac2a11197d2e63cb113b036a6f1d2dc8f88
eunkeeee <eunkeeeee@gmail.com>
2022-08-12 16:05:34 +0900

    Design) 시계 초 없앰

--- js/clock.js
+++ js/clock.js
@@ -4,16 +4,16 @@ function getClock() {
   const date = new Date();
   let AMPM = "AM";
   let hours = date.getHours();
-  if (hours < 12) {
-    AMPM = "AM";
-    hours = String(hours).padStart(2, "0");
-  } else {
-    AMPM = "PM";
-    hours = String(hours - 12).padStart(2, "0");
-  }
+  // if (hours < 12) {
+  //   AMPM = "AM";
+  //   hours = String(hours).padStart(2, "0");
+  // } else {
+  //   AMPM = "PM";
+  //   hours = String(hours - 12).padStart(2, "0");
+  // }
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds} ${AMPM}`;
+  clock.innerText = `${hours}:${minutes}`;
 }
 
 getClock();
